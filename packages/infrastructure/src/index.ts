4c3944b152354f71138991cd335bb877496666c5
agent69420pm <fjewenm98o@protonmail.com>
2026-07-04 11:28:20 +0000

    feat(infra): define DocumentRepository contract interface

--- packages/infrastructure/src/index.ts
+++ packages/infrastructure/src/index.ts
@@ -1,4 +1,5 @@
 export * from './db/db.js';
+export * from './db/repositories/document-repository.js';
 export * from './db/schema/documents.js';
 export * from './storage/local-storage.js';
 export * from './storage/storage-service.js';

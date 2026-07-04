66186f8a08b538204aaa89a38c52ad4e1329e424
agent69420pm <fjewenm98o@protonmail.com>
2026-07-04 11:28:42 +0000

    feat(infra): implement SqliteDocumentRepository using Drizzle SQLite

--- packages/infrastructure/src/index.ts
+++ packages/infrastructure/src/index.ts
@@ -1,5 +1,6 @@
 export * from './db/db.js';
 export * from './db/repositories/document-repository.js';
+export * from './db/repositories/sqlite-document-repository.js';
 export * from './db/schema/documents.js';
 export * from './storage/local-storage.js';
 export * from './storage/storage-service.js';
